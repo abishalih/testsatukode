23f116ed086575eeb6bb4bffc918b5733461628e
abishalih <habibullahshalihuddin@gmail.com>
2023-05-09 04:13:00 +0700

    feature: create service to get user list

---
+++ test-case/src/app/services/auth-service.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) { }
+
+  authLogin(): Observable<any[]> {
+    let url = this.apiUrl;
+    return this.http.get<any[]>(url);
+  }
+} 
