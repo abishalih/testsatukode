5f4628f8ad3b7c73180d41e88eb1a2dba610acca
abishalih <habibullahshalihuddin@gmail.com>
2023-05-09 04:15:05 +0700

    feature: add login button and display name if isLogin

--- test-case/src/app/pages/home/home.component.ts
+++ test-case/src/app/pages/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  authPayload = {
+    isLogin: false,
+    name: '',
+    password: '',
+    username: ''
+  };
 
+  constructor() { }
+  
+  ngOnInit() {
+    // Retrieve the saved username from localStorage
+    const isLogin = localStorage.getItem('isLogin')==="true"?true:false;
+    const name = localStorage.getItem('name') || "";
+    const password = localStorage.getItem('password') || "";
+    const username = localStorage.getItem('username') || "";
+
+    this.authPayload = {
+      isLogin,
+      name,
+      password,
+      username,
+    }
+    console.log({authPayload:this.authPayload});
+    
+  }
 }
